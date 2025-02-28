0604be596e6181d405390a5aacd0dec36ca3e3f5
GAlekseyV <galekseyv@gmail.com>
2025-02-28 19:19:26 +0300

    Add a solution to task 1 of topic 1.

--- Part_1/0_priority_container/priority_collection_2_1.cpp
+++ Part_1/0_priority_container/priority_collection_2_1.cpp
@@ -1,9 +1,9 @@
 #include "test_runner.h"
 #include <algorithm>
-#include <iostream>
+#include <cstddef>
 #include <iterator>
-#include <memory>
 #include <set>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -13,36 +13,71 @@ template<typename T>
 class PriorityCollection
 {
 public:
-  using Id = /* тип, используемый для идентификаторов */;
+  using Id = size_t;
 
   // Добавить объект с нулевым приоритетом
   // с помощью перемещения и вернуть его идентификатор
-  Id Add(T object);
+  Id Add(T object)
+  {
+    objects.emplace_back(std::move(object), 0);
+    sorted_objects.emplace(0, last_id);
+    return Id(last_id++);
+  }
 
   // Добавить все элементы диапазона [range_begin, range_end)
   // с помощью перемещения, записав выданные им идентификаторы
   // в диапазон [ids_begin, ...)
   template<typename ObjInputIt, typename IdOutputIt>
-  void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin);
+  void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin)
+  {
+    while (range_begin != range_end) {
+      *ids_begin++ = Add(std::move(*range_begin++));
+    }
+  }
 
   // Определить, принадлежит ли идентификатор какому-либо
   // хранящемуся в контейнере объекту
-  bool IsValid(Id id) const;
+  [[nodiscard]] bool IsValid(Id id) const
+  {
+    return objects[id].second >= 0;
+  }
 
   // Получить объект по идентификатору
-  const T &Get(Id id) const;
+  [[nodiscard]] const T &Get(Id id) const
+  {
+    return objects[id].first;
+  }
 
   // Увеличить приоритет объекта на 1
-  void Promote(Id id);
+  void Promote(Id id)
+  {
+    auto old_priority = objects[id].second++;
+    auto new_priority = old_priority + 1;
+    sorted_objects.erase({ old_priority, id });
+    sorted_objects.emplace(new_priority, id);
+  }
 
   // Получить объект с максимальным приоритетом и его приоритет
-  pair<const T &, int> GetMax() const;
+  [[nodiscard]] pair<const T &, int> GetMax() const
+  {
+    return objects[prev(sorted_objects.end())->second];
+  }
 
   // Аналогично GetMax, но удаляет элемент из контейнера
-  pair<T, int> PopMax();
+  pair<T, int> PopMax()
+  {
+    auto id = prev(sorted_objects.end())->second;
+    auto result = std::move(objects[id]);
+    sorted_objects.erase({ result.second, id });
+    objects[id] = make_pair(T(), -1);
+    return result;
+  }
 
 private:
   // Приватные поля и методы
+  set<pair<int, Id>> sorted_objects;
+  vector<pair<T, int>> objects;
+  size_t last_id = 0;
 };
 
 
